08570b4801c295f557690a0285b9803db620687c
iyegoroff <iegoroff@gmail.com>
2024-09-14 03:56:46 +0300

    osx support

--- .config/tmux-powerline/segments/online_status.sh
+++ .config/tmux-powerline/segments/online_status.sh
@@ -1,10 +1,26 @@
 # Print online status
 
-run_segment() {
+platform=$(uname)
+
+if [[ $platform == "Darwin" ]]; then
+  run_segment() {
+    if ($(ping -c 1 -t 3 www.google.com >/dev/null 2>&1)); then
+      echo "󰖩"
+    else
+      echo "󰤯"
+    fi
+
+  	return 0
+  }
+else
+  run_segment() {
     if ($(ping -c 1 -w 3 www.google.com >/dev/null 2>&1)); then
       echo "󰖩"
     else
       echo "󰤯"
     fi
-	return 0
-}
+
+  	return 0
+  }
+fi
+
--- .config/tmux-powerline/segments/volume.sh
+++ .config/tmux-powerline/segments/volume.sh
@@ -1,6 +1,20 @@
 # Print sound volume
 
-run_segment() {
+platform=$(uname)
+
+if [[ $platform == "Darwin" ]]; then
+  run_segment() {
+    local volume=$(osascript -e 'output volume of (get volume settings)')
+    if [[ $volume == "0" ]]; then
+      echo " 󰝟 "
+    else
+      echo "󰕾 $volume"
+    fi
+
+    return 0
+  }
+else
+  run_segment() {
     local info=$(amixer get PCM)
     local volume=$(echo $info | paste -s | sed 's/.*\[\([0-9]*\)%.*/\1/')
     local mute=$(echo $info | grep "\[off\]")
@@ -10,5 +24,7 @@ run_segment() {
     else
       echo "󰕾 $volume"
     fi
-	return 0
-}
+
+  	return 0
+  }
+fi
