1ffb3f5173590af5b7530a55c8db141c1d322f83
iyegoroff <iegoroff@gmail.com>
2025-10-29 20:16:18 +0300

    mem_used

--- .config/tmux-powerline/segments/mem_use.sh
+++ .config/tmux-powerline/segments/mem_use.sh
@@ -1,26 +1,52 @@
-# Print memory usage
+# shellcheck shell=bash
+# Prints memory usage
+# In context of this segment "1 GB" means 1073741824 bytes.
 
-platform=$(uname)
+TMUX_POWERLINE_SEG_MEM_USED_ICON_DEFAULT=" "
 
-if [[ $platform == "Darwin" ]]; then
-  run_segment() {
-    local mem=$(\
-        vm_stat \
-        | perl -ne '/page size of (\d+)/ and $size=$1; /Pages\s+([^:]+)[^\d]+(\d+)/ and printf("%-16s % 16.2f Mi\n", "$1:", $2 * $size / 1000000000);' \
-        | grep "^active\|wired\|compressor|\speculative" \
-        | grep -o '[0-9\.]*' \
-        | paste -sd+ - \
-        | bc -l \
-    )
-    echo " ${mem} G"
+generate_segmentrc() {
+	read -r -d '' rccontents <<EORC
+# Memory icon
+export TMUX_POWERLINE_SEG_MEM_USED_ICON="${TMUX_POWERLINE_SEG_MEM_USED_ICON_DEFAULT}"
+EORC
+	echo "$rccontents"
+}
+
+run_segment() {
+  __process_settings
+  
+  if tp_shell_is_macos; then
+    local stats
+    local page_size
+    local free_pages
+    local external_pages
+    local hw_memsize
+    local mem
+    
+    stats=$(vm_stat)
+    page_size=$(echo $stats | sed -e 's/.*page size of \([0-9]*\).*/\1/')
+    hw_memsize=$(sysctl hw.memsize | sed -e 's/^hw.memsize: \([0-9*]\)/\1/')
+    free_pages=$(echo $stats | sed -e 's/.*Pages free: \([0-9]*\).*/\1/')
+    external_pages=$(echo $stats | sed -e 's/.*File-backed pages: \([0-9]*\).*/\1/')
+    mem=$(echo "($hw_memsize - ($free_pages + $external_pages) * $page_size) / 1073741824" | bc -l)
+  
+    echo "${TMUX_POWERLINE_SEG_MEM_USED_ICON}$(__round $mem 2) GB"
     return 0
-  }
 
-else
-  run_segment() {
-    # free -hL --mega | sed -e 's/.*MemUse\s*\([0-9\.]*\)\(\S*\)\s*MemFree.*/ \1 \2/'
+  elif tp_shell_is_linux; then
     free -h --mega | grep Mem | sed -e 's/Mem:\s*[0-9\.]*\w*\s*\([0-9\.]*\)*\(\S*\).*/ \1 \2/'
     return 0
-  }
-fi
+  fi
+}
+
+__process_settings() {
+	if [ -z "$TMUX_POWERLINE_SEG_MEM_USED_ICON" ]; then
+		export TMUX_POWERLINE_SEG_MEM_USED_ICON="${TMUX_POWERLINE_SEG_MEM_USED_ICON_DEFAULT}"
+	fi
+}
+
+# source https://askubuntu.com/a/179949
+__round() {
+  printf "%.$2f" "$(echo "scale=$2;(((10^$2)*$1)+0.5)/(10^$2)" | bc)"
+};
 
